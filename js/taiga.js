09a3e4469b5741c0c00c942b193fdcc7e089af9d
Eren Okka <erenokka@gmail.com>
2017-03-14 08:04:03 +0300

    Clean up and improve various things

--- js/taiga.js
+++ js/taiga.js
@@ -1,23 +1,3 @@
-$("#features-link").click(function() {
-  $('#main-tab a[href="#features"]').tab('show')
-});
-
-$("#screenshots-link").click(function() {
-  $('#main-tab a[href="#screenshots"]').tab('show')
-});
-
-$("#support-link").click(function() {
-  $('#main-tab a[href="#support"]').tab('show')
-});
-
 $(document).ready(function(){
-  $('a[href="' + window.location.hash + '"]').click()
+  $('a[href="' + window.location.hash + '"]').click();
 });
-
-/*
-var download_version = $('#download-version');
-var url = 'data/taiga.json';
-$.getJSON(url, function(data) {
-  download_version.html('v' + data['version']);
-});
-*/
