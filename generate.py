da3d19a2a63b73d7526801850f768d0e9b25ef82
Eren Okka <erenokka@gmail.com>
2019-11-20 14:15:08 +0300

    Build sitemap

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import json
+import xml.etree.ElementTree as ET
 
 from datetime import datetime
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -22,28 +23,51 @@ def save_html(html, name):
     with open(f'public/{name}.html', 'w', encoding='utf-8') as file:
         file.write(html)
 
-env = Environment(
-    loader=FileSystemLoader('templates'),
-    autoescape=select_autoescape(['html'])
-)
+def build_pages(pages):
+    env = Environment(
+        loader=FileSystemLoader('templates'),
+        autoescape=select_autoescape(['html'])
+    )
 
-pages = ['api', 'index', 'latest']
+    features = load_docs('features')
+    quotes = load_data('quotes')
+    screenshots = load_data('screenshots')
+    support = {
+        'faq': load_docs('faq'),
+        'issues': load_docs('issues'),
+        'how-it-works': load_docs('how-it-works'),
+    }
+    version = datetime.now().strftime('%Y%m%d%H%M%S')
 
-features = load_docs('features')
-quotes = load_data('quotes')
-screenshots = load_data('screenshots')
-support = {
-    'faq': load_docs('faq'),
-    'issues': load_docs('issues'),
-    'how-it-works': load_docs('how-it-works'),
-}
-version = datetime.now().strftime('%Y%m%d%H%M%S')
+    for name in pages:
+        template = env.get_template(f'{name}.html')
+        html = template.render(features=features,
+                               quotes=quotes,
+                               screenshots=screenshots,
+                               support=support,
+                               version=version)
+        save_html(html, name)
 
-for name in pages:
-    template = env.get_template(f'{name}.html')
-    html = template.render(features=features,
-                           quotes=quotes,
-                           screenshots=screenshots,
-                           support=support,
-                           version=version)
-    save_html(html, name)
+def build_sitemap(pages):
+    urlset = ET.Element('urlset')
+    urlset.set('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9')
+    for page in pages:
+        url = ET.SubElement(urlset, 'url')
+        loc = ET.SubElement(url, 'loc')
+        path = '/' if page == 'index' else f'/{page}.html'
+        loc.text = f'https://taiga.moe{path}'
+        lastmod = ET.SubElement(url, 'lastmod')
+        lastmod.text = datetime.now().strftime('%Y-%m-%dT%H:%M:%S%z')
+        if page == 'index':
+            priority = ET.SubElement(url, 'priority')
+            priority.text = '1.0'
+    tree = ET.ElementTree(urlset)
+    tree.write('public/sitemap.xml', encoding='utf-8', xml_declaration=True)
+
+def main():
+    pages = ['index', 'api', 'latest']
+    build_pages(pages)
+    build_sitemap(pages)
+
+if __name__ == "__main__":
+    main()
